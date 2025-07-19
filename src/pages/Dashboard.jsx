8ce9da07dc300e4157522a0186b223d23e4ed7e6
EricEthan2020 <mdaytharlay@gmail.com>
2025-07-19 17:23:34 +0630

    test

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import Container from '../component/Container'
-import Hero from '../component/Hero'
-import Footer from '../component/Footer'
-import Header from '../component/Header'
-import Video from"../component/Video";
-import Ourservices from '../component/Ourservices'
-import Slider from '../component/Slider'
-import OurClients from '../component/OurClients'
+import React from "react";
+import Container from "../component/Container";
+import Hero from "../component/Hero";
+import Footer from "../component/Footer";
+import Header from "../component/Header";
+import Video from "../component/Video";
+import Ourservices from "../component/Ourservices";
+import Slider from "../component/Slider";
+import OurClients from "../component/OurClients";
 const DashBoard = () => {
   return (
     <Container>
-      <Header/>
-      <Hero/>
-      <Video/>
-      <Ourservices/>
-      <OurClients/>
-      <Slider/>
-      <Footer/>
+      <Header />
+      <Hero />
+      <Video />
+      <Ourservices />
+      <Footer />
     </Container>
-  )
-}
+  );
+};
 
-export default DashBoard
+export default DashBoard;
