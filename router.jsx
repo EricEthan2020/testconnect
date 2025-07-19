65c63983cf7939994c89e36b2c92fb67316e8971
EricEthan2020 <zawlwinoo1932020@gmail.com>
2025-07-20 00:24:21 +0630

    testcode

--- router.jsx
+++ router.jsx
@@ -1,32 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
-import DashBoard from "./src/pages/DashBoard";
-import App from "./src/App";
 import About from "./src/pages/About";
 import Contact from "./src/pages/Contact";
 import Work from "./src/pages/Work";
 import Layout from "./src/component/Layout";
+import DashBoard from "./src/pages/Dashboard";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-                children: [
-                {
-                    path: "/",
-                    element: <DashBoard/>
-                },
-            {
-            path: "/about",
-            element: <About/>,
-        },
-        {
-            path: "/contact",
-            element: <Contact/>,
-        },
-        {
-            path: "/work",
-            element: <Work/>
-        }
-    ]
-    }
-])
+  {
+    path: "/",
+    element: <DashBoard />,
+    children: [
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/work",
+        element: <Work />,
+      },
+    ],
+  },
+]);
 export default router;
